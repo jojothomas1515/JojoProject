32ba501f6fac91bfacdad285205846c8af86082a
jojothomas1515 <jojothomas1515@gmail.com>
2022-05-26 06:58:35 +0100

    added post detail page

--- blog/views.py
+++ blog/views.py
@@ -16,3 +16,13 @@ def index(req):
         pass
 
     return render(req, 'blog/index.html', context=context)
+
+@login_required(login_url='login')
+def view_post(req, pk):
+    post = blogpost.objects.get(pk=pk)
+    context = {'post': post.Post}
+    if req.user.is_staff:
+        context['is_admin'] = True
+    else:
+        pass
+    return render(req, 'blog/post.html', context=context)
