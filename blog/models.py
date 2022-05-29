f371d22fa242365ac6584b61e77fc49d8d19f793
jojothomas1515 <jojothomas1515@gmail.com>
2022-05-29 19:59:47 +0100

    made some changes

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from tinymce import models as tmodel
 
 # Create your models here.
 
 class blogpost(models.Model):
     title = models.CharField(max_length=200 )
-    Post = models.TextField()
+    Post = models.TextField(null=True, blank=True)
     logo = models.ImageField('head image', null=True, blank=True)
-    summary = models.CharField(max_length=30, null=True, blank=True)
+    summary = models.CharField(max_length=200, null=True, blank=True)
     pub_date = models.DateField('Date Published', auto_now=True)
     pub_time = models.TimeField('Time Published', auto_now=True)
 
