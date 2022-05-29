ddbd43ba01ae840c2ccbac06f7afb2c93a983664
jojothomas1515 <jojothomas1515@gmail.com>
2022-05-29 05:55:02 +0100

    trying to fix imagefeild

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from tinymce import models as tmodel
 class blogpost(models.Model):
     title = models.CharField(max_length=200 )
     Post = tmodel.HTMLField()
-    logo = models.ImageField('head image', null=True, blank=True)
+    logo = models.ImageField('head image', null=True, blank=True, upload_to='./upload/')
     summary = models.CharField(max_length=30, null=True, blank=True)
     pub_date = models.DateField('Date Published', auto_now=True)
     pub_time = models.TimeField('Time Published', auto_now=True)
