4d3673eb54cacaa6254216a0458d7761aade3201
jojothomas1515 <jojothomas1515@gmail.com>
2022-07-13 15:17:08 +0100

    delete post functionality

--- JojoProject/settings.py
+++ JojoProject/settings.py
@@ -81,8 +81,9 @@ WSGI_APPLICATION = 'JojoProject.wsgi.application'
 if DEBUG:
     DATABASES = {
 
-        'default': {
-
+         'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
 
     }
@@ -165,5 +166,5 @@ elif not DEBUG:
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
--- blog/static/blog/js/prof.js
+++ blog/static/blog/js/prof.js
@@ -1,6 +1,9 @@
 const open_edit = document.querySelector('[data-user-edit-icon]')
 const close_edit = document.querySelector('[data-user-edit-close]')
 const image_image = document.querySelector('[data-input-image]')
+const opt_icon = document.querySelector('.opt-icon')
+const option = document.querySelector('.option')
+const delete_post_button = document.querySelector("#delete-post")
 
 open_edit.addEventListener('click', () => {
     const modal = document.querySelector('.modal')
@@ -14,16 +17,53 @@ close_edit.addEventListener('click', () => {
     modal.style.pointerEvents = 'none';
 })
 
-image_image.addEventListener('change', (e)=>{
+image_image.addEventListener('change', (e) => {
     let img = e.target.files[0]
 
     const fr = new FileReader()
-    fr.onload =function (e){
-        document.getElementById('mi-c').src=e.target.result
+    fr.onload = function (e) {
+        document.getElementById('mi-c').src = e.target.result
     }
     fr.readAsDataURL(img)
 })
 
 
+opt_icon.addEventListener('click', e => {
+
+    if (option.style.display === 'block') option.style.display = "none"
+    else option.style.display = 'block'
+
+})
+// opt_icon.addEventListener('mouseleave', e=>{
+//         opt_icon.classList.remove('fa-close')
+//
+// })
+
+
+document.addEventListener('click', e => {
+    if(e.target.classList.contains('opt-icon') | e.target.classList.contains('option')| e.target.classList.contains('option-items')) return
+    else option.style.display = "none"
+})
+
+delete_post_button.addEventListener('click', e=> delete_post(e))
+const delete_post = async e =>{
+    const mv = getCsrfToken()
+    console.log(mv)
+    const post_id = e.target.dataset.postid
+    // fetch('deletePost', {
+    //     method:'DELETE',
+    //     headers:{
+    //         "CSRFToken" : ""
+    //     }
+    // })
+}
+
+function getCsrfToken(){
+    const mv=  document.cookie.split(";").filter((value, index) =>{
+        return value.match('(csrftoken)+.*')
+            });
+    const result =mv.toString().split('=').pop()
+    return result
+}
 
 
