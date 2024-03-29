c4386eadb0ca092a6108c813589fd665946f1ff9
Joseph <jojothomas1515@gmail.com>
2024-03-29 19:45:41 +0100

    some update to ui and fixed tinymce not showing

--- JojoProject/settings.py
+++ JojoProject/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@o7p4mm0p%#i9@@#!c6j4ww#6(u5d*c6)8b94ji4xrsosacjx4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://jojopage.urbangeek.tech', 'http://jojopage.urbangeek.tech']
 
 if DEBUG:
-    ALLOWED_HOSTS = [ 'localhost', '127.0.0.1',]
+    ALLOWED_HOSTS = [ 'localhost', '127.0.0.1','https://jojopage.urbangeek.tech', 'http://jojopage.urbangeek.tech']
 else:
     ALLOWED_HOSTS = ['https://jojopage.urbangeek.tech', 'http://jojopage.urbangeek.tech']
 
